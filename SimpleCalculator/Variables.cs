0f34b14faf266862ff8cf0eb20f94b78a76492bc
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-19 18:31:57 -0500

    raw files setup and initial method and properties

---
+++ SimpleCalculator/Variables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace SimpleCalculator
+{
+    public class Variables
+    {
+        string pattern = @"^(?<variable>[a-z]{1})\s*\=\s*(?<value>\-?[0-9]+)$";
+
+        public bool variablePatternMath(string input)
+        {
+            Match match = Regex.Match(input, pattern);
+            if (match.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
